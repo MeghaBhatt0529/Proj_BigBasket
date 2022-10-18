417f7a55136f0669c75bf66cb0f9919d6bafe2ec
MeghaBhatt0529 <meghabhatt0529@gmail.com>
2022-10-18 16:59:38 +0530

    code

--- bb.js
+++ bb.js
@@ -1,3 +1,6 @@
+
+// Coupon //
+
 function loadCoupon(){
     document.getElementById('coupon').style.visibility = 'visible';
     document.getElementById('main').style.opacity='0.7'
@@ -7,3 +10,11 @@ function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
     document.getElementById('main').style.opacity='1'
 }
+
+// Light/Dark //
+
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle('mydark')
+}
+
