0cb0ae6704f5b2506696f582c944dfbf7d76052b
MeghaBhatt0529 <meghabhatt0529@gmail.com>
2022-10-26 19:58:41 +0530

    geolocation

--- bb.js
+++ bb.js
@@ -1,15 +1,14 @@
 
 // // Coupon //
-
 function loadCoupon() {
-document.getElementById('coupon').style.visibility = 'visible';
-document.getElementById('main').style.opacity = '0.7'
-}
-
-function closeCoupon() {
-document.getElementById('coupon').style.visibility = 'hidden';
-document.getElementById('main').style.opacity = '1'
-}
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.7'
+    }
+    
+    function closeCoupon() {
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1'
+    }
 
 // Light-Dark //
 
